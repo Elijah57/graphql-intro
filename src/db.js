314b27063fd3beb5d883624e940971f9d4dcd572
Elijah Echekwu <elijahechekwu30@gmail.com>
2025-02-17 07:35:09 +0100

    source files

---
+++ src/db.js
@@ -0,0 +1,6 @@
+export let users = [
+    {id: 1, name: "Emeka Ndubuisi", email: "smart@gmail.com", age: 22},
+    {id: 2, name: "Kelvin Mark", email: "kevlaar@gmail.com", age: 23}
+]
+
+// db.js file is going to simulate a database, just for learning purposes.
